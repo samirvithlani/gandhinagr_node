b2eae07d8df8cd421e67f21e70519ebebd113caf
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-07 14:29:48 +0530

    delete

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,44 +1,82 @@
 //require user model..
-const userSchema = require("../models/UserModel")
+const userSchema = require("../models/UserModel");
 
 const getAllUsers = async (req, res) => {
-    const users= await userSchema.find()
-    res.json({
-        message:"List of users",
-        data:users
-    })
-
-}
+  const users = await userSchema.find();
+  res.json({
+    message: "List of users",
+    data: users,
+  });
+};
 const getUserById = async (req, res) => {
-    const user = await userSchema.findById(req.params.id)
-    if(user){
+  const user = await userSchema.findById(req.params.id);
+  if (user) {
+    res.json({
+      message: "User found",
+      data: user,
+    });
+  } else {
+    res.json({
+      message: "User not found",
+    });
+  }
+};
+
+const addUser = async (req, res) => {
+  //console.log("req.body..",req.body)
+  const savedUser = await userSchema.create(req.body);
+  res.json({
+    message: "User added successfully",
+    data: savedUser,
+  });
+  //res.send("ok...")
+};
+
+const deleteUser = async (req, res) => {
+  //id..
+  const id = req.params.id;
+  const deletedUser = await userSchema.findByIdAndDelete(id);
+  if (deletedUser) {
+    res.json({
+      message: "User deleted successfully",
+      data: deletedUser,
+    });
+  } else {
+    res.json({
+      message: "User not found",
+    });
+  }
+};
+//req.query example
+
+const deleteUserByName = async (req, res) => {
+
+    console.log("req.query..",req.query)
+    const name = req.query.name;
+    const deletedUsers = await userSchema.deleteMany({name:name})
+    if(deletedUsers.deletedCount>0){
+
         res.json({
-            message:"User found",
-            data:user
+            message:"Users deleted successfully",
+            data:deletedUsers
         })
+
     }
     else{
         res.json({
-            message:"User not found"
+            message:"Users not found",
         })
     }
-}
 
-const addUser = async(req,res)=>{
-
-    //console.log("req.body..",req.body)
-    const savedUser  = await userSchema.create(req.body)
-    res.json({
-        message:"User added successfully",
-        data:savedUser
-    })
-    //res.send("ok...")
 
 
 }
-    
+
+
 module.exports = {
-    getAllUsers,
-    getUserById,
-    addUser
-}
+  getAllUsers,
+  getUserById,
+  addUser,
+  deleteUser,
+  deleteUserByName
+};
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const userController = require('../controller/UserController');
 router.get("/users",userController.getAllUsers)
 router.get("/user/:id",userController.getUserById)
+router.delete("/user/:id",userController.deleteUser)
 router.post("/user",userController.addUser)
+router.delete("/userbyname",userController.deleteUserByName)
 
 module.exports = router;
